f26e0b6c99b6a79ffc30e072c41374be237e582b
Hamit Can SAYILGAN <hamitcansayilgan@gmail.com>
2024-03-10 18:17:53 +0300

    - Feature: Random number finding method and control feature added

--- Form1.cs
+++ Form1.cs
@@ -10,23 +10,76 @@ using System.Windows.Forms;
 
 namespace WinFormAppRandomNumberGuessing
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void btnBaslat_Click(object sender, EventArgs e)
-        {
-        }
+		int rastgele, sayac;
 
-        private void btnKontrol_Click(object sender, EventArgs e)
-        {
-        }
+		private void btnBaslat_Click(object sender, EventArgs e)
+		{
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        }
-    }
+			//MessageBox.Show("Başlat!");
+			// TEST: label1.Text = rastgele.ToString();
+
+			btnKontrol.Enabled = true;
+			btnKontrol.BackColor = Color.PeachPuff;
+			Random random = new Random();
+			rastgele = random.Next(0, 100); // Static OLMAYAN Metod
+			sayac = 10;
+			lblSayac.Text = sayac.ToString() + " Hakkınız Var";
+			lblDurum.Text = "";
+			txtSayi.Text = "";
+
+
+		}
+
+		private void btnKontrol_Click(object sender, EventArgs e)
+		{
+			if (txtSayi.Text == "")
+			{
+				lblDurum.Text = "SAYI GİRİNİZ";
+			}
+			else
+			{
+				int girilenSayi = Convert.ToInt32(txtSayi.Text);
+				if (girilenSayi < rastgele)
+				{
+					lblDurum.Text = "Arttır";
+					sayac--;
+				}
+				else if (girilenSayi > rastgele)
+				{
+					lblDurum.Text = "Azalt";
+					sayac--;
+				}
+				else
+				{
+					lblDurum.Text = "TEBRİKLER KAZANDINIZ!";
+					btnKontrol.Enabled = false;
+				}
+			}
+
+			lblSayac.Text = sayac.ToString() + " Hakkınız Kaldı!";
+
+
+			if (sayac == 0)
+			{
+				lblDurum.Text = "KAZANAMADINIZ, TEKRAR DENEYİNİZ.";
+				btnKontrol.Enabled = false;
+
+
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			btnKontrol.Enabled = false;
+			lblSayac.Text = "10 Hakkınız Var!";
+
+		}
+	}
 }
